ef4197383e81047a889ca406b9085c6ba23ff2d7
Scott Simpson <sasimpson@gmail.com>
2021-12-02 08:01:35 -0600

    Day 1, Part 2

--- main.go
+++ main.go
@@ -20,28 +20,45 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	data, err := readData(scanner)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	prev = 0
 	current = 0
 	increases = 0
-	for scanner.Scan() {
-		current, err = strconv.ParseInt(scanner.Text(), 10, 32)
-		if err != nil {
-			log.Fatal(err)
-		}
+
+	for i := 2; i < len(data); i++ {
+		window := data[i-2] + data[i-1] + data[i]
 
 		if prev != 0 {
-			if current > prev {
+			if window > prev {
 				increases++
 			}
 		}
-
-		prev = current
+		prev = window
 	}
 
 	fmt.Println("total increases:", increases)
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+}
+
+func readData(scanner *bufio.Scanner) ([]int64, error) {
+	var data []int64
+
+	for scanner.Scan() {
+		line, err := strconv.ParseInt(scanner.Text(), 10, 32)
+		if err != nil {
+			return []int64{}, err
+		}
+
+		data = append(data, line)
 	}
+
+	if err := scanner.Err(); err != nil {
+		return []int64{}, err
+	}
+
+	return data, nil
 }
