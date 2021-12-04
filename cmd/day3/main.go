524d132e5952929c1818ba626e1e3e74e251d4f9
Scott Simpson <sasimpson@gmail.com>
2021-12-04 07:53:23 -0600

    Day 3, part 2

--- cmd/day3/main.go
+++ cmd/day3/main.go
@@ -13,33 +13,124 @@ type power struct {
 	epsilon uint
 }
 
-func main() {
+func (p *power) consumption() uint {
+	return p.gamma * p.epsilon
+}
 
-	var p power
-
-	data, err := readData("data/diagnostics.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func (p *power) analyze(d diagnostics) {
 	for i := 11; i >= 0; i-- {
 		var notbit uint
-		bit := mostCommonBit(data, i)
+		bit := mostCommonBit(d.Readings, i)
 		if bit == 0 {
 			notbit = 1
 		}
-		fmt.Println("place:", i, " bit:", bit, " not bit:", notbit)
 		p.gamma = p.gamma << 1
 		p.gamma = p.gamma + bit
 		p.epsilon = p.epsilon << 1
 		p.epsilon = p.epsilon + notbit
 	}
-
-	fmt.Printf("gamma result: \t%012b\n", p.gamma)
-	fmt.Printf("epsilon result: \t%012b\n", p.epsilon)
-	fmt.Printf("power consumption: %d", p.gamma*p.epsilon)
 }
 
+type lifeSupport struct {
+	oxygen uint
+	co2    uint
+}
+
+func (ls *lifeSupport) analyzeOxygen(d diagnostics) {
+	data := new(diagnostics)
+	for _, val := range d.Readings {
+		data.Readings = append(data.Readings, val)
+	}
+	readings(data, 11, true)
+	ls.oxygen = uint(data.Readings[0])
+}
+
+func (ls *lifeSupport) analyzeCO2(d diagnostics) {
+	data := new(diagnostics)
+	for _, val := range d.Readings {
+		data.Readings = append(data.Readings, val)
+	}
+	readings(data, 11, false)
+	ls.co2 = uint(data.Readings[0])
+}
+
+func (ls *lifeSupport) rating() uint {
+	return ls.oxygen * ls.co2
+}
+
+func readings(d *diagnostics, level int, most bool) int {
+	var deleteList []int
+	var compBit uint
+	if most {
+		compBit = mostCommonBit(d.Readings, level)
+	} else {
+		compBit = leastCommonBit(d.Readings, level)
+	}
+	for i, r := range d.Readings {
+		b := getBit(r, level)
+		if b != int64(compBit) {
+			deleteList = append(deleteList, i)
+		}
+	}
+	for i, dl := range deleteList {
+		d.delete(dl - i)
+	}
+	if level <= 0 || len(d.Readings) == 1 {
+		return 0
+	}
+	level--
+	return readings(d, level, most)
+}
+
+type diagnostics struct {
+	Readings []int64
+}
+
+func (d *diagnostics) load(filename string) error {
+	file, _ := os.Open(filename)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line, err := strconv.ParseInt(scanner.Text(), 2, 16)
+		if err != nil {
+			return err
+		}
+		d.Readings = append(d.Readings, line)
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *diagnostics) delete(i int) int64 {
+	x := d.Readings[i]
+	d.Readings = append(d.Readings[:i], d.Readings[i+1:]...)
+	return x
+}
+
+func main() {
+	var (
+		diags diagnostics
+		p     power
+		ls    lifeSupport
+	)
+
+	err := diags.load("data/diagnostics.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p.analyze(diags)
+	fmt.Printf("power consumption: %d\n", p.consumption())
+
+	ls.analyzeOxygen(diags)
+	ls.analyzeCO2(diags)
+	fmt.Printf("life support rating: %d\n", ls.rating())
+}
+
+// mostCommonBit - get most common bit for a position in a byte.
 func mostCommonBit(data []int64, pos int) uint {
 	var ones int
 	for i := 0; i < len(data); i++ {
@@ -53,25 +144,20 @@ func mostCommonBit(data []int64, pos int) uint {
 	return 0
 }
 
+func leastCommonBit(data []int64, pos int) uint {
+	var ones int
+	for i := 0; i < len(data); i++ {
+		if getBit(data[i], pos) == 1 {
+			ones++
+		}
+	}
+	if ones < len(data)-ones {
+		return 1
+	}
+	return 0
+}
+
 func getBit(data int64, pos int) int64 {
 	shifted := data >> pos
 	return shifted & 1
-}
-
-func readData(filename string) (data []int64, err error) {
-	file, _ := os.Open(filename)
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line, err := strconv.ParseInt(scanner.Text(), 2, 16)
-		if err != nil {
-			return data, err
-		}
-		data = append(data, line)
-	}
-	if err := scanner.Err(); err != nil {
-		return data, err
-	}
-	return
 }
--- cmd/day3/main_test.go
+++ cmd/day3/main_test.go
@@ -90,7 +90,7 @@ func Test_mostCommonBit(t *testing.T) {
 			},
 			want: 1,
 		}, {
-			name: "msb test 3",
+			name: "msb test 4",
 			args: args{
 				data: []int64{2, 0, 3, 0, 2, 0},
 				pos:  1,
@@ -102,6 +102,55 @@ func Test_mostCommonBit(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := mostCommonBit(tt.args.data, tt.args.pos); got != tt.want {
 				t.Errorf("mostCommonBit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_leastCommonBit(t *testing.T) {
+	type args struct {
+		data []int64
+		pos  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint
+	}{
+		{
+			name: "lsb test 1",
+			args: args{
+				data: []int64{1, 1, 0, 0, 2, 3, 5},
+				pos:  0,
+			},
+			want: 0,
+		}, {
+			name: "lsb test 2",
+			args: args{
+				data: []int64{1, 0, 0, 0, 2, 3, 0},
+				pos:  0,
+			},
+			want: 1,
+		}, {
+			name: "lsb test 3",
+			args: args{
+				data: []int64{2, 0, 3, 0, 2, 3, 0},
+				pos:  1,
+			},
+			want: 0,
+		}, {
+			name: "lsb test 4",
+			args: args{
+				data: []int64{2, 0, 3, 0, 2, 0},
+				pos:  1,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := leastCommonBit(tt.args.data, tt.args.pos); got != tt.want {
+				t.Errorf("leastCommonBit() = %v, want %v", got, tt.want)
 			}
 		})
 	}
