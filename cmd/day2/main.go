9939bf50b60c4a5ec946225f5a0c32644f5e7104
Scott Simpson <sasimpson@gmail.com>
2021-12-04 08:16:38 -0600

    Day 2 cleanup

--- cmd/day2/main.go
+++ cmd/day2/main.go
@@ -18,36 +18,13 @@ type position struct {
 	distance int
 	depth    int
 	aim      int
+	data     []move
 }
 
-func main() {
-	data, err := readData("data/course.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pos := position{}
-
-	for _, m := range data {
-		switch m.Direction {
-		case "forward":
-			pos.distance += m.Value
-			pos.depth += pos.aim * m.Value
-		case "up":
-			pos.aim -= m.Value
-		case "down":
-			pos.aim += m.Value
-		}
-	}
-
-	fmt.Println("distance * depth:", pos.distance*pos.depth)
-
-}
-
-func readData(filename string) (data []move, err error) {
+func (p *position) load(filename string) error {
 	file, err := os.Open(filename)
 	if err != nil {
-		return data, err
+		return err
 	}
 	defer file.Close()
 
@@ -56,18 +33,44 @@ func readData(filename string) (data []move, err error) {
 		line := strings.Split(scanner.Text(), " ")
 		value, err := strconv.ParseInt(line[1], 10, 64)
 		if err != nil {
-			return []move{}, err
+			return err
 		}
 		motion := move{
 			Direction: line[0],
 			Value:     int(value),
 		}
 
-		data = append(data, motion)
+		p.data = append(p.data, motion)
 	}
 
 	if err := scanner.Err(); err != nil {
-		return []move{}, err
+		return err
 	}
-	return
+	return nil
+}
+
+func (p *position) analyze() {
+	for _, m := range p.data {
+		switch m.Direction {
+		case "forward":
+			p.distance += m.Value
+			p.depth += p.aim * m.Value
+		case "up":
+			p.aim -= m.Value
+		case "down":
+			p.aim += m.Value
+		}
+	}
+}
+
+func main() {
+	pos := position{}
+	err := pos.load("data/course.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pos.analyze()
+	fmt.Println("distance * depth:", pos.distance*pos.depth)
+
 }
